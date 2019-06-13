f0165484f039320faaacfa9fc988c3f079b0dfb7
Conor Green <conorgreen@mmc.school.nz>
2019-06-14 10:39:06 +1200

    Introduction of Bitmap images, lists and ints to be used in future

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -12,6 +12,21 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+        Random RandomNumber;  // create a new random number generator
+        Bitmap OGShipImage; // create a new variable as the orignal ship image incase ship image is damaged during rotation
+        Bitmap ShipImage; // create a new variable as the ship image 
+        Ship myShip; // create new variable called my ship
+        Bitmap OGEnemyImage; // create a new vairable called original Enemy image incase Enemy image gets damaged during rotation
+        Bitmap OGBulletImage; // create a new variable called original bullet image incse bullet image gets damaged during rotation
+        List<Enemy> EnemyList; // create a new list variable called Enemylist ( this is to hold the many Enemies used on the form)
+        List<Bullet> bulletList; // create a new list for the bullets on the game panel and call it bullet list
+
+        int EnemySpeed;
+
+        int Points, lives;
+
+        const int maxEnemies = 3;
+
         public Form1()
         {
             InitializeComponent();
